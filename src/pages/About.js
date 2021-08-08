e94e918ecc488f87666a39966390ef595d86706d
LeonMher <99.p3rc3n7@gmail.com>
2021-08-08 11:52:28 -0500

    another try baby

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
             <h1>About me</h1>
             <p>Hi! I'm Mher from Armenia and I was born and raised in Alaverdi.
                 I love my hometown and I'm ready to share everything you need to know
-                before visiting Alaverdi!
+                before visiting Alaverdi!Visit Armenia ;)
             </p>
         </div>
     )
